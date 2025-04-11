4698bb2c8c4adea79725b6d4cf35189e82d5689a
Matteo Negri <matteo@matteonegri.eu>
2025-04-11 11:59:22 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
   readonly VITE_GOOGLE_FONTS_API_KEY: string;
+  readonly VITE_OPENROUTER_API_KEY: string;
+
 }
 
 interface ImportMeta {
